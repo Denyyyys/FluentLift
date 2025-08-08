6b2f6b7ed3bd84f746fc48d79f8a9f7923bc37e6
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-08 03:35:36 +0200

    feat(backend): add Spring Boot backend with Dockerfile and update docker-compose.yml

---
+++ backend/src/main/java/com/denyyyys/langLearner/LanguageLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.denyyyys.langLearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearnerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/denyyyys/langLearner/LanguageLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.denyyyys.langLearner;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class LanguageLearnerApplicationTests {
+
+// @Test
+// void contextLoads() {
+// }
+
+// }
