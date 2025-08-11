031749e72c6ba43aac8cd255178e7cf668280a5a
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-11 04:33:38 +0200

    feat(backend): implement DTOs, entities and mappers for decks and cards

---
+++ backend/src/main/java/com/denyyyys/langLearner/model/postgres/entity/Card.java
@@ -0,0 +1,41 @@
+package com.denyyyys.langLearner.model.postgres.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String frontText;
+
+    @Column(nullable = false)
+    private String backText;
+
+    @Column(nullable = false)
+    private boolean isArchived = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "deck_id", nullable = false)
+    @JsonBackReference
+    private Deck deck;
+}
