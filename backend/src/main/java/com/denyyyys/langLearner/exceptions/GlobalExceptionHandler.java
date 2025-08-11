c7a4579d67921bb8b0ee0da9065c71352af335fc
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-11 04:36:03 +0200

    feat(backend): create custom exceptions

--- backend/src/main/java/com/denyyyys/langLearner/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/denyyyys/langLearner/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.denyyyys.langLearner.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,7 +23,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<String> handleUsernameNotFoundException(UserAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ex.getMessage());
+                .body("User already exists: " + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
@@ -29,6 +34,30 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handleException(AuthenticationException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed: " + ex.getMessage());
+    }
+
+    // TODO: check how it works via debugger
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<String> handleException(DuplicateResourceException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Rerource already exist: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleException(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<String> handleException(ResourceNotFound ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Resource not found: " + ex.getMessage());
     }
 
 }
