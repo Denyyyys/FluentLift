e1b15e41656a2043890f5b45d15456691ef862db
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-11 04:37:19 +0200

    feat(backend): setup repositories for decks and cards

---
+++ backend/src/main/java/com/denyyyys/langLearner/repo/postgres/CardRepository.java
@@ -0,0 +1,8 @@
+package com.denyyyys.langLearner.repo.postgres;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.denyyyys.langLearner.model.postgres.entity.Card;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
