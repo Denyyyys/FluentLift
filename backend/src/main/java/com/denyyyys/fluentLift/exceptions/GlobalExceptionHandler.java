92d77020b221f1080e263c5b11c0f981218c10bd
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-26 14:25:55 +0200

    fix(backend): move general exception handler to bottom of class to prevent it from overriding specific handlers

--- backend/src/main/java/com/denyyyys/fluentLift/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/denyyyys/fluentLift/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.denyyyys.fluentLift.exceptions;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -26,28 +23,19 @@ public class GlobalExceptionHandler {
                 .body("User already exists: " + ex.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong: " + ex.getMessage());
-    }
-
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handleException(AuthenticationException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed: " + ex.getMessage());
     }
 
-    // TODO: check how it works via debugger
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return ResponseEntity.badRequest().body(errors);
+    public ResponseEntity<String> handleException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     @ExceptionHandler(DuplicateResourceException.class)
     public ResponseEntity<String> handleException(DuplicateResourceException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Rerource already exist: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Resource already exist: " + ex.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
@@ -58,6 +46,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFound.class)
     public ResponseEntity<String> handleException(ResourceNotFound ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Resource not found: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong: " + ex.getMessage());
     }
 
 }
