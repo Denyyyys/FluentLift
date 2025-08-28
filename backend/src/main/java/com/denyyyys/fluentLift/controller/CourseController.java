0c1c4ee32150e556c1b0b5a75cedb6dffb381bbb
Denyyyys <denys.fokashchuk@gmail.com>
2025-08-28 23:51:01 +0200

    feat(backend): add service and controller method for course creation

--- backend/src/main/java/com/denyyyys/fluentLift/controller/CourseController.java
+++ backend/src/main/java/com/denyyyys/fluentLift/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.denyyyys.fluentLift.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.denyyyys.fluentLift.model.postgres.dto.request.course.CourseCreateDto;
+import com.denyyyys.fluentLift.model.postgres.entity.course.Course;
 import com.denyyyys.fluentLift.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,9 +23,10 @@ public class CourseController {
     private final CourseService courseService;
 
     @PostMapping
-    public String createCourse(@AuthenticationPrincipal UserDetails user,
+    public ResponseEntity<Course> createCourse(@AuthenticationPrincipal UserDetails user,
             @Validated @RequestBody CourseCreateDto courseDto) {
+        Course course = courseService.createCourse(courseDto, user.getUsername());
 
-        return "yoo" + courseDto.toString() + user.getUsername();
+        return ResponseEntity.status(HttpStatus.CREATED).body(course);
     }
 }
--- backend/src/main/java/com/denyyyys/fluentLift/service/CourseService.java
+++ backend/src/main/java/com/denyyyys/fluentLift/service/CourseService.java
@@ -1,7 +1,14 @@
 package com.denyyyys.fluentLift.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.denyyyys.fluentLift.exceptions.ResourceNotFound;
+import com.denyyyys.fluentLift.model.postgres.dto.request.course.CourseCreateDto;
+import com.denyyyys.fluentLift.model.postgres.entity.AppUser;
+import com.denyyyys.fluentLift.model.postgres.entity.course.Course;
+import com.denyyyys.fluentLift.model.postgres.mapper.CourseMapper;
+import com.denyyyys.fluentLift.repo.postgres.AppUserRepository;
 import com.denyyyys.fluentLift.repo.postgres.CourseRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,5 +17,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final AppUserRepository appUserRepository;
 
+    @Transactional
+    public Course createCourse(CourseCreateDto dto, String creatorEmail) {
+        Course course = CourseMapper.toEntity(dto);
+        AppUser creator = appUserRepository.findByEmail(creatorEmail)
+                .orElseThrow(() -> new ResourceNotFound("Creator not found"));
+        course.setCreator(creator);
+
+        courseRepository.save(course);
+        return course;
+    }
 }
