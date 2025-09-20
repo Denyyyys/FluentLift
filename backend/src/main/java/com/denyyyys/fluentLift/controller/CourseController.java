e398aa2216435cbd05ef1c8fa8c7900605583aa7
Denyyyys <denys.fokashchuk@gmail.com>
2025-09-20 19:05:16 +0200

    feat(backend): add course enrollment action in controller and service

--- backend/src/main/java/com/denyyyys/fluentLift/controller/CourseController.java
+++ backend/src/main/java/com/denyyyys/fluentLift/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.denyyyys.fluentLift.model.postgres.dto.request.course.CourseCreateDto;
 import com.denyyyys.fluentLift.model.postgres.dto.response.CourseResponseDto;
+import com.denyyyys.fluentLift.model.postgres.dto.response.UserCourseEnrollmentResponseDto;
 import com.denyyyys.fluentLift.model.postgres.entity.course.Course;
 import com.denyyyys.fluentLift.service.CourseService;
 
@@ -43,5 +45,19 @@ public class CourseController {
     public ResponseEntity<List<CourseResponseDto>> getCreatedByMeCourses(@AuthenticationPrincipal UserDetails user) {
         return ResponseEntity.ok().body(courseService.getCreatedByMeCourses(user.getUsername()));
     }
+
+    @PostMapping("/{courseId}/users")
+    public ResponseEntity<UserCourseEnrollmentResponseDto> enroll(@AuthenticationPrincipal UserDetails user,
+            @PathVariable Long courseId) {
+        UserCourseEnrollmentResponseDto responseDto = courseService.enroll(user.getUsername(), courseId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    // @GetMapping("/me/enrolled")
+    // public ResponseEntity<List<CourseResponseDto>>
+    // getEnrolledCourses(@AuthenticationPrincipal UserDetails user) {
+    // return
+    // ResponseEntity.ok().body(courseService.getEnrolledCourses(user.getUsername()));
+    // }
 
 }
--- backend/src/main/java/com/denyyyys/fluentLift/service/CourseService.java
+++ backend/src/main/java/com/denyyyys/fluentLift/service/CourseService.java
@@ -1,18 +1,25 @@
 package com.denyyyys.fluentLift.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.denyyyys.fluentLift.exceptions.ForbiddenActionException;
+import com.denyyyys.fluentLift.exceptions.ResourceAlreadyExistsException;
 import com.denyyyys.fluentLift.exceptions.ResourceNotFound;
 import com.denyyyys.fluentLift.model.postgres.dto.request.course.CourseCreateDto;
 import com.denyyyys.fluentLift.model.postgres.dto.response.CourseResponseDto;
+import com.denyyyys.fluentLift.model.postgres.dto.response.UserCourseEnrollmentResponseDto;
 import com.denyyyys.fluentLift.model.postgres.entity.AppUser;
 import com.denyyyys.fluentLift.model.postgres.entity.course.Course;
+import com.denyyyys.fluentLift.model.postgres.entity.course.UserCourseEnrollment;
 import com.denyyyys.fluentLift.model.postgres.mapper.CourseMapper;
+import com.denyyyys.fluentLift.model.postgres.mapper.UserCourseEnrollmentMapper;
 import com.denyyyys.fluentLift.repo.postgres.AppUserRepository;
 import com.denyyyys.fluentLift.repo.postgres.CourseRepository;
+import com.denyyyys.fluentLift.repo.postgres.UserCourseEnrollmentRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final UserCourseEnrollmentRepository enrollmentRepository;
     private final AppUserRepository appUserRepository;
 
     @Transactional
@@ -42,11 +50,45 @@ public class CourseService {
     }
 
     public List<CourseResponseDto> getCreatedByMeCourses(String creatorEmail) {
-        List<Course> courses = courseRepository.findAllByCreatorEmail(creatorEmail);
+        List<Course> courses = courseRepository.findAllByCreator_Email(creatorEmail);
         List<CourseResponseDto> coursesResponse = courses.stream().map(course -> CourseMapper.toResponseDto(course))
                 .toList();
 
         return coursesResponse;
     }
+
+    public UserCourseEnrollmentResponseDto enroll(String userEmail, Long courseId) {
+        AppUser user = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new ResourceNotFound("User not found"));
+
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ResourceNotFound("Course not found"));
+
+        if (user.getId().equals(course.getCreator().getId())) {
+            throw new ForbiddenActionException("Course creator cannot enroll in their own course.");
+        }
+
+        Optional<UserCourseEnrollment> enrollmentFromRepo = enrollmentRepository.findByCourseAndUser(course, user);
+
+        if (enrollmentFromRepo.isPresent()) {
+            throw new ResourceAlreadyExistsException("Enrollment for provided user already exists");
+        }
+
+        UserCourseEnrollment enrollment = new UserCourseEnrollment();
+
+        enrollment.setCourse(course);
+        enrollment.setUser(user);
+        enrollment = enrollmentRepository.save(enrollment);
+
+        return UserCourseEnrollmentMapper.toResponseDto(enrollment, "enrolled");
+    }
+
+    // public List<CourseResponseDto> getEnrolledCourses(String userEmail) {
+    // List<Course> courses = courseRepository.findAllByCreatorEmail(creatorEmail);
+    // List<CourseResponseDto> coursesResponse = courses.stream().map(course ->
+    // CourseMapper.toResponseDto(course))
+    // .toList();
+
+    // return coursesResponse;
+    // }
 
 }
