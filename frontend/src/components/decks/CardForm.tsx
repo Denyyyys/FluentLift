cc8903cadf1ceca552ccea6d2f38f138ab5ce513
Denyyyys <denys.fokashchuk@gmail.com>
2025-12-08 18:49:53 +0100

    feat(frontend): add component for creating a new card in deck creation

--- frontend/src/components/decks/CardForm.tsx
+++ frontend/src/components/decks/CardForm.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import type { CardCreateDto } from '../../types/card'
+import { FaTrashAlt } from "react-icons/fa";
 
-function CardForm() {
+type CardFormProps = {
+    newCard: CardCreateDto,
+    updateCard: (updatedNewCard: CardCreateDto) => void
+    deleteCard: (id: string) => void
+}
+
+function CardForm({ newCard, updateCard, deleteCard }: CardFormProps) {
+
     return (
-        <div>CardForm</div>
+        <div className='mb-3 p-3'>
+            <p >
+                <FaTrashAlt color='red' size={20} className="clickable" onClick={() => deleteCard(newCard.id)} />
+            </p>
+
+            <div className="mb-3">
+                <label htmlFor="frontText" className="form-label">Front Text</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="frontText"
+                    value={newCard.frontText}
+                    onChange={(e) => updateCard({ ...newCard, frontText: e.target.value })}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="backText" className="form-label">Back Text</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="backText"
+                    value={newCard.backText}
+                    onChange={(e) => updateCard({ ...newCard, backText: e.target.value })}
+                />
+            </div>
+        </div>
     )
 }
 
