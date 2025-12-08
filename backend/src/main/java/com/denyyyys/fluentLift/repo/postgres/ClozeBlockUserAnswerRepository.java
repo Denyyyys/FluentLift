37b611724e3ce2944440161a1e418965ccfac29b
Denyyyys <denys.fokashchuk@gmail.com>
2025-12-08 18:32:23 +0100

    fix(backend): fix bad name for some methods in ClozeBlockUserAnswerRepository repo

--- backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/ClozeBlockUserAnswerRepository.java
+++ backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/ClozeBlockUserAnswerRepository.java
@@ -3,9 +3,11 @@ package com.denyyyys.fluentLift.repo.postgres;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.denyyyys.fluentLift.model.postgres.entity.AppUser;
 import com.denyyyys.fluentLift.model.postgres.entity.course.lessonBlock.ClozeBlockUserAnswer;
 
 public interface ClozeBlockUserAnswerRepository extends JpaRepository<ClozeBlockUserAnswer, Long> {
@@ -14,7 +16,10 @@ public interface ClozeBlockUserAnswerRepository extends JpaRepository<ClozeBlock
                 WHERE cbua.user.id = :userId AND
                 cbua.clozeBlockAnswer.cloze.lesson.unit.course.id = :courseId
             """)
-    List<ClozeBlockUserAnswer> findForUserIdAndCourseId(@Param("userId") Long userId,
+    List<ClozeBlockUserAnswer> findAllByUserIdAndCourseId(@Param("userId") Long userId,
             @Param("courseId") Long courseId);
+
+    @Modifying
+    void deleteAllByUserAndClozeBlockAnswer_Cloze_LessonId(AppUser user, Long lessonId);
 
 }
