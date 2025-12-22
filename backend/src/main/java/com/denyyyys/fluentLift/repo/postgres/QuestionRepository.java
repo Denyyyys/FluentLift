bdf44872223cae6f09b753c54a14117183631466
Denyyyys <denys.fokashchuk@gmail.com>
2025-12-22 07:47:28 +0100

    feat(backend): add fetching list of questions by field "solved"

--- backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/QuestionRepository.java
+++ backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/QuestionRepository.java
@@ -1,5 +1,9 @@
 package com.denyyyys.fluentLift.repo.postgres;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +15,9 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     @Query("""
             SELECT q FROM Question q
             LEFT JOIN FETCH q.answers a
-            LEFT JOIN FETCH a.replies r
             WHERE q.id = :id
             """)
-    Question findQuestionWithAllAnswers(@Param("id") Long id);
+    Optional<Question> findQuestionWithAllAnswers(@Param("id") Long id);
 
+    List<Question> findAllBySolved(boolean solved, Pageable pageable);
 }
