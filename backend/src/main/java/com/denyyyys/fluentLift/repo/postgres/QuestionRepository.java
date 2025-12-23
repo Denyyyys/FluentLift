d833b4413cb5269d840304188af75fb5d3af41ad
Denyyyys <denys.fokashchuk@gmail.com>
2025-12-23 07:48:51 +0100

    feat(backend): add querying questions by content

--- backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/QuestionRepository.java
+++ backend/src/main/java/com/denyyyys/fluentLift/repo/postgres/QuestionRepository.java
@@ -13,29 +13,29 @@ import com.denyyyys.fluentLift.model.postgres.entity.questionsAndAnswers.Questio
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    @Query("""
-            SELECT q FROM Question q
-            LEFT JOIN FETCH q.answers a
-            WHERE q.id = :id
-            """)
-    Optional<Question> findQuestionWithAllAnswers(@Param("id") Long id);
+        @Query("""
+                        SELECT q FROM Question q
+                        LEFT JOIN FETCH q.answers a
+                        WHERE q.id = :id
+                        """)
+        Optional<Question> findQuestionWithAllAnswers(@Param("id") Long id);
 
-    List<Question> findAllBySolved(boolean solved, Pageable pageable);
+        List<Question> findAllBySolved(boolean solved, Pageable pageable);
 
-    @Query("""
-            SELECT q FROM Question q
-            LEFT JOIN q.tags t
-            WHERE (:tags IS NULL OR t IN :tags)
-                AND (:query IS NULL OR q.title LIKE CONCAT('%', :query, '%'))
-                AND (:isSolved IS NULL OR q.solved = :isSolved)
-            GROUP BY (q)
-            HAVING COUNT(DISTINCT t) >= :tagCount
-            """)
-    Page<Question> searchQuestions(
-            @Param("query") String query,
-            @Param("tags") List<String> tags,
-            @Param("tagCount") long tagCount,
-            @Param("isSolved") Boolean isSolved,
-            Pageable pageable);
+        @Query("""
+                        SELECT q FROM Question q
+                        LEFT JOIN q.tags t
+                        WHERE (:tags IS NULL OR t IN :tags)
+                            AND (:query IS NULL OR q.title LIKE CONCAT('%', :query, '%') OR q.content LIKE CONCAT('%', :query, '%'))
+                            AND (:isSolved IS NULL OR q.solved = :isSolved)
+                        GROUP BY (q)
+                        HAVING COUNT(DISTINCT t) >= :tagCount
+                        """)
+        Page<Question> searchQuestions(
+                        @Param("query") String query,
+                        @Param("tags") List<String> tags,
+                        @Param("tagCount") long tagCount,
+                        @Param("isSolved") Boolean isSolved,
+                        Pageable pageable);
 
 }
