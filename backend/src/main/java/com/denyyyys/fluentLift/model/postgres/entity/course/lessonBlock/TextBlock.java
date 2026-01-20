85b691eec23979d9ad29f6cfef5318e0ee707c2f
Denyyyys <denys.fokashchuk@gmail.com>
2026-01-20 16:55:23 +0100

    fix(backend): change max length of text block to 1500 characters

--- backend/src/main/java/com/denyyyys/fluentLift/model/postgres/entity/course/lessonBlock/TextBlock.java
+++ backend/src/main/java/com/denyyyys/fluentLift/model/postgres/entity/course/lessonBlock/TextBlock.java
@@ -4,6 +4,7 @@ import com.denyyyys.fluentLift.model.postgres.entity.course.Lesson;
 import com.denyyyys.fluentLift.model.postgres.enums.TextBlockType;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,7 +43,7 @@ public class TextBlock {
     @Enumerated(EnumType.STRING)
     private TextBlockType type;
 
-    @Column(nullable = false)
+    @Column(nullable = true, length = 1500)
     private String text;
 
 }
