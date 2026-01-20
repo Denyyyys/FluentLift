964fc11421d5309750ff8f89fc46b1903bba1866
Denyyyys <denys.fokashchuk@gmail.com>
2026-01-20 17:00:50 +0100

    chore(backend): delete unused imports

--- backend/src/main/java/com/denyyyys/fluentLift/model/postgres/entity/course/lessonBlock/TextBlock.java
+++ backend/src/main/java/com/denyyyys/fluentLift/model/postgres/entity/course/lessonBlock/TextBlock.java
@@ -4,7 +4,6 @@ import com.denyyyys.fluentLift.model.postgres.entity.course.Lesson;
 import com.denyyyys.fluentLift.model.postgres.enums.TextBlockType;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
