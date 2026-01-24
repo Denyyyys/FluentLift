8022946e6e183d6915b4b739450916fc7c46de2b
Denyyyys <denys.fokashchuk@gmail.com>
2026-01-24 17:23:25 +0100

    feat(mobile): add all necessary image assets

---
+++ mobile/app/src/main/java/com/example/navigation/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.navigation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Primary100 = Color(0xFFE6EFEF)
+val Primary200 = Color(0xFFb9dddf)
+val Primary300 = Color(0xFF8bbfc2)
+val Primary400 = Color(0xFF83adaf)
+val Primary500 = Color(0xFF6a9c9e)
+val Primary600 = Color(0xFF518c8e)
+val Primary700 = Color(0xFF387b7e)
+val Primary800 = Color(0xFF1f6b6e)
+val Primary900 = Color(0xFF065a5e)
+
+
