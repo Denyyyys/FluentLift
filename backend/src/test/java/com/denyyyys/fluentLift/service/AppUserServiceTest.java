fd099facc0bc2810bb49a033563f60ca6e064485
Denyyyys <denys.fokashchuk@gmail.com>
2026-02-11 16:27:08 +0100

    test(backend): add unit test for adding user to see if GitHub Actions works properly

--- backend/src/test/java/com/denyyyys/fluentLift/service/AppUserServiceTest.java
+++ backend/src/test/java/com/denyyyys/fluentLift/service/AppUserServiceTest.java
@@ -2,7 +2,9 @@ package com.denyyyys.fluentLift.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.denyyyys.fluentLift.exceptions.UserAlreadyExistsException;
 import com.denyyyys.fluentLift.model.postgres.dto.request.UserRegistrationRequestDto;
 import com.denyyyys.fluentLift.model.postgres.entity.AppUser;
 import com.denyyyys.fluentLift.repo.postgres.AppUserRepository;
@@ -56,5 +59,22 @@ public class AppUserServiceTest {
         verify(repository).save(any(AppUser.class));
     }
 
+    @Test
+    void addUser_shouldThrowException_whenEmailExists() {
+        UserRegistrationRequestDto dto = new UserRegistrationRequestDto(
+                "John",
+                "john@test.com",
+                "password"
+        );
+
+        when(repository.findByEmail(dto.getEmail()))
+                .thenReturn(Optional.of(new AppUser()));
+
+        assertThrows(UserAlreadyExistsException.class,
+                () -> service.addUser(dto));
+
+        verify(repository, never()).save(any());
+        verify(encoder, never()).encode(any());
+    }
 
 }
